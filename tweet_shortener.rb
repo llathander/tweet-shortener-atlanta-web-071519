90e42a13b7eb6fe7af03258e71af371c73e650a1
Adrian Samaniego <llathander@gmail.com>
2019-07-12 17:41:39 +0000

    Automatically backed up by Learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -1,57 +1,58 @@
-phrase = "Hey guys, can anyone teach me how to be cool? I really want to be the best at everything, you know what I mean? Tweeting is super fun you guys!!!!"
+def dictionary 
+  dict = {
+    "hello" => 'hi',
+    "to"=> "2",
+    "too"=> "2",
+    "two" => "2",
+    "for"=>"4",
+    "four" => "4", 
+    "For"=>"4",
+    "Four" => "4",
+    'be' => "b",
+    'you'=> "u",
+    "at" =>"@",
+    "and" => "&",
+    }
+end 
 
-# Write your code here.
-require "pry"
-
-def dictionary
-  convert = {
-   "hello" => "hi",
-   "to" => "2",
-   "two" => "2",
-   "too" => "2",
-   "for" => "4",
-   "four" => "4",
-   "be" => "b",
-   "you" => "u",
-   "at" => "@",
-   "and" => "&"
-}
-end
 
 def word_substituter(tweet)
-  # replaces long words with their expected short form
+  dict = dictionary
+  words = tweet.split(" ")
 
-    tweet.split(" ").map do |word|
-      if dictionary.keys.include?(word.downcase)
-        word = dictionary[word.downcase]
-      else
-        word
+  words.collect do |word|
+    dictionary.collect do |key, value|
+      if word == key 
+        word.replace(value)
+      end 
     end
-  end.join(" ")
-end
+  end 
+  words.join(" ")
+end 
 
-def bulk_tweet_shortener(tweet)
-  tweet.map do |phrase|
-    puts word_substituter(phrase)
+
+def bulk_tweet_shortener(tweets_array)
+
+  tweets_array.collect do |tweet|
+    puts word_substituter(tweet)
   end
-end
+end 
+
 
 def selective_tweet_shortener(tweet)
-  tweet.split(" ").map do |phrase|
-    if phrase.length > 140
-      word_substituter(phrase)
-    elsif phrase.length <= 140
-      phrase
-    end
-  end
-end
+  if tweet.chars.length > 140  
+    word_substituter(tweet)
+  else 
+    tweet 
+  end 
+end 
+
+
 
 def shortened_tweet_truncator(tweet)
-  tweet.split(" ").map do |phrase|
-    if phrase.length > 140
-      word_substituter(phrase)[0..140] + "..."
-    else
-      phrase
-    end
-  end.join(" ")
-end
+  if word_substituter(tweet).chars.length > 140  
+    truncated = word_substituter(tweet).chars[0..136].push("...").join("")
+  else 
+    word_substituter(tweet) 
+  end 
+end 
